79720bf933b3f4023b739c5eb9bf7f679fe2c506
Luis Bardalez <lbardalez@gmail.com>
2022-11-20 12:42:03 -0500

    [green] 127.0.0.1 passes without changes

--- validate-ipv4-address.test.js
+++ validate-ipv4-address.test.js
@@ -19,4 +19,10 @@ describe('IP Address Validator', () => {
         const expected = true
         expect(result).toBe(expected)
     })
+    test('127.0.0.1 should return true', () => {
+        const input = '127.0.0.1'
+        const result = validateIpv4Address(input)
+        const expected = true
+        expect(result).toBe(expected)
+    })
 })
