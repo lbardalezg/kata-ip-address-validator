2bf72a617cdc8b8ec79110c253fe21fcae1cf813
Luis Bardalez <lbardalez@gmail.com>
2022-11-22 23:01:17 -0500

    [green] 10.0.1 return false

--- validate-ipv4-address.js
+++ validate-ipv4-address.js
@@ -1,5 +1,6 @@
 function validateIpv4Address(input) {
     const bytes = input.split('.')
+    if(bytes.length !== 4) return false
     if(bytes[3] === '0') return false
     if(bytes[3] === '255') return false
     return true
